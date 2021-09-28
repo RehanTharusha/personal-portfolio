4e75df76ca139679487f9ec1a95443c76381872b
rt4k <48168596+RehanTharusha@users.noreply.github.com>
2021-09-28 11:30:22 +0530

    implemented Projects section

---
+++ 1.0.0/main.js
@@ -0,0 +1,10 @@
+var projectButtonActive = document.getElementById('projectButton');
+var projectContainerRun = document.getElementById('projectContainer');
+
+projectButton.addEventListener('click', showProjectContainer);
+
+function showProjectContainer(){
+    projectContainerRun.style.display = 'block';
+    
+};
+

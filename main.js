ddf6ebc0517c131c52a8301233104a53b329c169
rt4k <48168596+RehanTharusha@users.noreply.github.com>
2021-09-30 14:57:37 +0530

    added Photography section

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 var projectButtonActive = document.getElementById('projectButton');
+var photographyButtonActive = document.getElementById('photographyButton');
 var projectContainerRun = document.getElementById('projectContainer');
+var photoContainerRun = document.getElementById('photographyContainer');
 
-projectButton.addEventListener('click', showProjectContainer);
+projectButtonActive.addEventListener('click', showProjectContainer);
+photographyButtonActive.addEventListener('click', showPhotoContainer);
 
 function showProjectContainer(){
     projectContainerRun.style.display = 'block';
-    
+};
+
+function showPhotoContainer(){
+    photoContainerRun.style.display = 'block';
 };
 
